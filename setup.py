a878eb9afa6dd5258bacabf830e910445219927e
Miguel Ballesteros <miballegh@outlook.com>
2020-04-24 15:55:36 +0200

    Fixed package namespace and setup findig

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.rst", "r") as fh:
     long_desc = fh.read()
 
 setup(
     name='factiva-core',
-    version='0.0.4',
+    version='0.0.6',
     description='Python package with root definitions and dictionaries, to support other functional packages.',
     long_description=long_desc,
     long_description_content_type='text/x-rst',
@@ -13,7 +13,7 @@ setup(
     author_email='miguel.ballesteros@dowjones.com',
     # Warning: the folder 'factiva' should NOT have an __init__.py file to avoid conflicts with the same namespace across other packages
     package_dir={'': 'src'},
-    packages=find_packages('src'),
+    packages=['factiva.core'],
     package_data={'': ['*.csv']},
     url='https://github.com/dowjones/factiva-core-python',
 
