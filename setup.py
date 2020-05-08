9469933d4f42b8cb9aeed2230e3eacdde881e67e
Miguel Ballesteros <miballegh@outlook.com>
2020-05-09 00:36:52 +0200

    Updated packages list and version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name='factiva-core',
-    version='0.0.7',
+    version='0.1.2',
     description='Python package with root definitions and dictionaries, to support other functional packages.',
     long_description=long_desc,
     long_description_content_type='text/x-rst',
@@ -13,7 +13,7 @@ setup(
     author_email='miguel.ballesteros@dowjones.com',
     # Warning: the folder 'factiva' should NOT have an __init__.py file to avoid conflicts with the same namespace across other packages
     package_dir={'': 'src'},
-    packages=['factiva.core'],
+    packages=['factiva.core', 'factiva.helper', 'factiva.core.dicts', 'factiva.core.const'],
     package_data={'': ['*.csv']},
     url='https://github.com/dowjones/factiva-core-python',
 
