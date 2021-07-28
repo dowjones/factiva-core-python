ab1bf1c86dee409a0d837f91da8523726c580a21
Miguel Ballesteros <miballegh@outlook.com>
2021-07-28 16:49:47 +0200

    Added project links

--- setup.py
+++ setup.py
@@ -17,14 +17,22 @@ setup(
     description='Python package with root definitions and dictionaries, to support other functional packages.',
     long_description=long_desc,
     long_description_content_type='text/x-rst',
+    license='MIT',
     author='Dow Jones Customer Engineers',
     author_email='customer.solutions@dowjones.com',
+
     # Warning: the folder 'factiva' should NOT have an __init__.py file to avoid conflicts with the same namespace across other packages
     package_dir={'': 'src'},
     packages=['factiva.core', 'factiva.helper', 'factiva.core.dicts', 'factiva.core.const',
         'factiva.core.auth', 'factiva.core.req', 'factiva.core.tools'],
     package_data={'': ['*.csv']},
-    url='https://github.com/dowjones/factiva-core-python',
+
+    url='https://developer.dowjones.com/',
+    project_urls={
+            "GitHub": "https://github.com/dowjones/factiva-core-python",
+            "Documentation": "https://factiva-core-python.readthedocs.io/",
+            "Bug Tracker": "https://github.com/dowjones/factiva-core-python/issues",
+        },
 
     classifiers=[  # Optional
         # How mature is this project? Common values are
