315dea84231bc45dc5a6ffa426bc7525a3e77236
Jose Pereira <jose.pereira@wizeline.com>
2022-01-26 12:37:11 -0500

    Add stream option on get request

--- src/factiva/core/req/__init__.py
+++ src/factiva/core/req/__init__.py
@@ -5,11 +5,11 @@ import requests, json
 from .. import const
 
 
-def send_get_request(endpoint_url=const.API_HOST, headers=None, qs_params=None):
+def send_get_request(endpoint_url=const.API_HOST, headers=None, qs_params=None, stream= False):
     """Send get request."""
     if (qs_params is not None) and (not isinstance(qs_params, dict)):
         raise ValueError('Unexpected qs_params value')
-    return requests.get(endpoint_url, headers=headers, params=qs_params)
+    return requests.get(endpoint_url, headers=headers, params=qs_params, stream=stream)
 
 
 def send_post_request(endpoint_url=const.API_HOST, headers=None, payload=None):
@@ -26,7 +26,7 @@ def send_post_request(endpoint_url=const.API_HOST, headers=None, payload=None):
     return requests.post(endpoint_url, headers=headers)
 
 
-def api_send_request(method='GET', endpoint_url=const.API_HOST, headers=None, payload=None, qs_params=None):
+def api_send_request(method='GET', endpoint_url=const.API_HOST, headers=None, payload=None, qs_params=None, stream= False):
     """Send a generic request to a certain API end point."""
     if headers is None:
         raise ValueError('Heders for Factiva requests cannot be empty')
@@ -36,7 +36,7 @@ def api_send_request(method='GET', endpoint_url=const.API_HOST, headers=None, pa
 
     try:
         if method == 'GET':
-            response = send_get_request(endpoint_url=endpoint_url, headers=headers, qs_params=qs_params)
+            response = send_get_request(endpoint_url=endpoint_url, headers=headers, qs_params=qs_params, stream=stream)
 
         elif method == 'POST':
             response = send_post_request(endpoint_url=endpoint_url, headers=headers, payload=payload)
